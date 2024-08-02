8bfb6e4eec7c88f261bcc362cd146989d60a68b5
trebours <tomarebours1@gmail.com>
2024-08-02 18:16:34 +0200

    add exoo

---
+++ 001/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trebours <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 12:47:30 by trebours          #+#    #+#             */
+/*   Updated: 2024/07/31 12:47:31 by trebours         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	std::string 	res;
+	std::string::iterator 	iter;
+
+	if (argc == 1) {
+		std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+		return (1);
+	}
+	for (int i = 1; argv[i]; i++) {
+		res += argv[i];
+		if (i < argc - 1)
+			res += ' ';
+	}
+	for (iter = res.begin(); iter < res.end(); ++iter)
+		*iter = std::toupper(*iter);
+	std::cout<< res <<std::endl;
+}
