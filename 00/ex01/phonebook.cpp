aae642caf72e8972679cace65b511f0589a4f193
trebours <tomarebours1@gmail.com>
2024-08-07 16:31:36 +0200

    add message in cpp 00/ex01

--- 00/ex01/phonebook.cpp
+++ 00/ex01/phonebook.cpp
@@ -10,14 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<iostream>
+#include <iostream>
+#include "phonebook.hpp"
+
+void	prompt_welcome()
+{
+	std::cout<<"****************************\n"
+			   "*   welcome in phonebook   *\n"
+			   "****************************\n"<<std::endl;
+}
+
+void	prompt_action()
+{
+	std::cout<<"****************************\n"
+			   "*    ADD = add contact     *\n"
+			   "* SEARCH = print a contact *\n"
+			   "*   EXIT = Exit phonebook  *\n"
+			   "****************************\n";
+}
+
+int	ft_action(std::string line)
+{
+	if (line.length() == 0)
+		return (1);
+	if (!line.compare("ADD"))
+		std::cout<<"ADD"<<std::endl;
+	else if (!line.compare("EXIT")) {
+		std::cout << "exit" << std::endl;
+		return (0);
+	} else
+		std::cout<<"Error input"<<std::endl;
+	return (-1);
+}
 
 int main()
 {
-	std::string prompt;
+	int 		i = -1;
+	std::string line;
 
-	for (int i = 0; i < 10 ; i++)
-		prompt += '*';
-	prompt += '\n';
-	for (int )
+	prompt_welcome();
+	prompt_action();
+	while (i == -1) {
+		std::cout<<"> ";
+		std::cin >> line;
+		i = ft_action(line);
+	}
 }
