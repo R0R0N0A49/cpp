b4b55868ef835b096f6e41b1f1b7c41fa7039415
trebours <tomarebours3@gmail.com>
2024-09-12 14:33:45 +0200

    first push

---
+++ 01/ex00/Zombie.hpp
@@ -0,0 +1,14 @@
+
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+# include <iostream>
+
+class Zombie {
+    public:
+        void    rename(std::string src);
+        void    announce();
+    private:
+}
+
+#endif
