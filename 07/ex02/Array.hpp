9b5f4eb9e679112318f1477c0f653069f228f332
trebours <tomarebours1@gmail.com>
2025-03-06 10:54:46 +0100

    add modif cpp07/ex02

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -10,27 +10,62 @@ class Array {
 		const unsigned long _size;
 
 	public:
-		Array() : _size(0) {};
-		Array(unsigned int n) : _size(n) { _content = new T[n];};
+		Array() : _size(0) { this->_content = NULL; };
+
+		Array(unsigned long n) : _size(0){
+			const_cast<unsigned long&>(_size) = n;
+			this->_content = new(std::nothrow) T[n];
+			if (!this->_content)
+				throw Fail(); // faire verifier avec un N = -
+		};
+
 		Array(Array const &copy) : _size(copy._size) {
-			_content = new T[copy._size];
+			_content = new(std::nothrow) T[copy._size];
+			if (!this->_content)
+				throw Fail();
 			for (unsigned long i = 0; i < _size; i++)
 				_content[i] = copy._content[i];
 		};
+
 		~Array() {
 			delete [] _content;
 		};
-		Array *operator=(Array const &copy) {
-			if (*this == copy)
+
+		Array<T> *operator=(Array<T> const &copy) {
+			if (this == copy)
 				return *this;
-			delete [] _content;
-			_content = new T[copy._size];
+			if (this->_content != NULL)
+				delete [] _content;
+			_content = new(std::nothrow) T[copy._size];
+			if (!this->_content)
+				throw Fail();
 			const_cast<unsigned long&>(_size) = copy._size;
 			for (unsigned long i = 0; i < _size; i++)
 				_content[i] = copy._content[i];
 			return *this;
 		};
-		T operator[](unsigned int src) { return _content[src];};
-		unsigned long size() {return _size; };
-		// add exception
+
+		T &operator[](unsigned long idx) {
+			if (idx >= this->_size) {
+				throw OutOfBounds();
+			}
+			return this->_content[idx];
+		};
+
+		unsigned long size() {
+			return _size;
+		};
+
+class OutOfBounds : public std::exception {
+	const char * what() const throw() {
+		return ("\033[1;38;2;200;50;75mIndex too big\033[1;m");
+	}
+};
+
+class Fail : public std::exception {
+	const char * what() const throw() {
+		return ("\033[1;38;2;200;50;75mAllocation fail\033[1;m");
+	}
+};
+
 };
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,7 +1,34 @@
 #include "Array.hpp"
 
 int main() {
-	Array<int> *tmp = new Array<int>(155);
-	int i = tmp[1];
-	tmp[1];
+	std::cout << "\033[1;38;2;0;150;175m" << "test with Array<string> and index too big" << "\033[1;m" << std::endl;
+	{
+		Array<std::string> tmp(2);
+		try {
+			tmp[0] = "bonjour";
+			tmp[1] = "hello";
+			std::cout << "tmp[0] = " << tmp[0] << std::endl;
+			std::cout << "tmp[1] = " << tmp[1] << std::endl;
+			std::cout << "tmp[2] = " << tmp[2] << std::endl; // throw error index
+		} catch (std::exception &e) {
+			std::cout << std::endl << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl << "\033[1;38;2;0;150;175m" << "test with Array<int> and size()" << "\033[1;m" << std::endl;
+	{
+		Array<int> *tmp = new Array<int>(10);
+		try {
+			std::cout << "tmp.size = " << tmp->size() << std::endl;
+			for (unsigned long i = 1; i < tmp->size(); i++) {
+				tmp[0][i] = tmp[0][i - 1] + i;
+			}
+			for (unsigned long i = 0; i < tmp->size(); i++) {
+				std::cout << "tmp[" << i << "] = " << tmp[0][i] << std::endl;
+			}
+		} catch (std::exception &e) {
+			delete tmp;
+			std::cout << std::endl << e.what() << std::endl;
+		}
+		delete tmp;
+	}
 }
