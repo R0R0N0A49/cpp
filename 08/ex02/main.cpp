d1cb222f14abe434fa03ab80f29ca0ca5de60ccd
trebours <tomarebours3@gmail.com>
2025-05-26 10:15:11 +0200

    change makefile and test on mutant stack

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -7,13 +7,14 @@ int main() {
 	MutantStack<int> mstack;
 	std::list<int> lstack;
 
+	std::cout << "Add 5 and 17 in MutantStack(mstack) and in list(lstack)" << std::endl;
 	mstack.push(5);
 	mstack.push(17);
 
 	lstack.push_back(5);
 	lstack.push_back(17);
 
-	std::cout << "First element: (mstack | lstack)" << std::endl;
+	std::cout << "\033[1;38;2;25;75;220mFirst element: (mstack | lstack)\033[1;m" << std::endl;
 	std::cout << mstack.top() << " | " << lstack.back() << std::endl;
 
 	mstack.pop();
@@ -22,40 +23,32 @@ int main() {
 
 	std::cout << std::endl;
 
-	std::cout << "size after one pop :" << std::endl;
+	std::cout << "\033[1;38;2;25;75;220msize after one pop :\033[1;m" << std::endl;
 	std::cout << mstack.size() << " | " << lstack.size() << std::endl;
+	std::cout << "\033[1;38;2;25;75;220mFirst element after one pop :\033[1;m" << std::endl;
+	std::cout << mstack.top() << " | " << lstack.back() << std::endl;
+
+	std::cout << std::endl << "Add 4 number in MutantStack(mstack) and in list(lstack)" << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 
 	lstack.push_back(3);
 	lstack.push_back(5);
 	lstack.push_back(737);
-	//[...]
 	lstack.push_back(0);
 
 	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-
 	std::list<int>::iterator lit = lstack.begin();
-	std::list<int>::iterator lite = lstack.end();
-
-	++it;
-	--it;
-
-	++lit;
-	--lit;
 
 	std::cout << std::endl;
-
-	std::cout << "Test iterator:" << std::endl;
-	while (it != ite)
+	std::cout << "\033[1;38;2;25;75;220mTest iterator:\033[1;m" << std::endl;
+	while (it != mstack.end())
 	{
 		std::cout << *it;
-		if (lit != lite) {
+		if (lit != lstack.end()) {
 			std::cout << " | " << *lit;
 			++lit;
 		}
